180e7d7965b579706911a9c89479e0c733832ae5
AndreGrigorian <ahgrigor75@gmail.com>
2023-12-19 13:19:06 -0800

    bogo working

---
+++ main.py
@@ -0,0 +1,33 @@
+import random
+
+
+def is_sorted(ls):
+    for i in range(len(ls)-1):
+        if ls[i] > ls[i + 1]:
+            return False
+    return True
+
+
+def bogo_sort(ls):
+    sorted_ls = list(ls)
+    while(not is_sorted(sorted_ls)):
+        random_indicies = []
+
+        for i in range(len(ls)):
+            random_index = random.randint(0, len(ls)-1)
+            # make sure there are no duplicate values in random_indicies
+            while(random_index in random_indicies):
+                random_index = random.randint(0, len(ls)-1)
+            random_indicies.append(random_index)
+
+        # print(random_indicies)
+
+        for i in range(len(ls)):
+            sorted_ls[random_indicies[i]] = ls[i]
+
+    return sorted_ls
+
+
+ls = [1, 2, 3, 4, 5]
+not_sorted = [3, 1, 90, 0, 34]
+print(bogo_sort(not_sorted))
