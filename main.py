938f5e74ff200fbdac623539d754b64fe507dcf4
AndreGrigorian <ahgrigor75@gmail.com>
2023-12-19 13:31:26 -0800

    documentation added

--- main.py
+++ main.py
@@ -9,10 +9,9 @@ def is_sorted(ls):
 
 
 def bogo_sort(ls):
-    sorted_ls = list(ls)
+    sorted_ls = list(ls)  # create copy to avoid affecting original list
     while(not is_sorted(sorted_ls)):
         random_indicies = []
-
         for i in range(len(ls)):
             random_index = random.randint(0, len(ls)-1)
             # make sure there are no duplicate values in random_indicies
@@ -20,7 +19,13 @@ def bogo_sort(ls):
                 random_index = random.randint(0, len(ls)-1)
             random_indicies.append(random_index)
 
-        # print(random_indicies)
+        # position each element of original list to its corresponding random index
+            # example:
+
+            # original List:             [9, 4, 0, 2]
+            # random indicies:           [2, 3, 0, 1]
+
+            # list after swapping:       [0, 2, 9, 4] (not sorted, so process will start again until sorted)
 
         for i in range(len(ls)):
             sorted_ls[random_indicies[i]] = ls[i]
